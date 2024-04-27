a5d73ebf69dd40406835cf0947172680a310ccfb
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-04-27 23:36:18 +0200

    feat(landing): position "malyvsen" on the left of menu items

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,34 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1>malyvsen</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        position: "absolute",
+        top: "25vh",
+        left: "25vw",
+        height: "50vh",
+        width: "50vw",
+        justifyContent: "center",
+        alignItems: "flex-end",
+      }}
+    >
+      <h1
+        style={{
+          writingMode: "vertical-rl",
+          rotate: "180deg",
+          margin: "0",
+          padding: "0",
+        }}
+      >
+        malyvsen
+      </h1>
+
       <div
         style={{
           display: "flex",
           flexDirection: "column",
-          position: "absolute",
-          top: "25vh",
-          left: "25vw",
-          height: "50vh",
-          width: "50vw",
           justifyContent: "center",
           alignItems: "flex-start",
         }}
@@ -21,7 +38,7 @@ function App() {
         <a href="/writer">writer</a>
         <a href="/programmer">programmer</a>
       </div>
-    </>
+    </div>
   );
 }
 
