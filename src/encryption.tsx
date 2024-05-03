ff68dc059282d85abca1e7c30bdd2463c56c8a13
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-05-03 21:05:53 +0200

    feat: add encryption

---
+++ src/encryption.tsx
@@ -0,0 +1,67 @@
+export async function encrypt({
+  password,
+  data,
+}: {
+  password: string;
+  data: string;
+}) {
+  const key = await passwordToKey(password);
+
+  const encryptedData = await crypto.subtle.encrypt(
+    { name: "AES-GCM", iv: initializationVector },
+    key,
+    textEncoder.encode(data)
+  );
+
+  return new Uint8Array(encryptedData);
+}
+
+export async function decrypt({
+  password,
+  encryptedData,
+}: {
+  password: string;
+  encryptedData: Uint8Array;
+}) {
+  const key = await passwordToKey(password);
+
+  const decryptedData = await crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: initializationVector },
+    key,
+    encryptedData
+  );
+
+  return textDecoder.decode(decryptedData);
+}
+
+async function passwordToKey(password: string): Promise<CryptoKey> {
+  const passwordBuffer = textEncoder.encode(password);
+
+  const keyMaterial = await crypto.subtle.importKey(
+    "raw",
+    passwordBuffer,
+    { name: "PBKDF2" },
+    false,
+    ["deriveKey"]
+  );
+  return await crypto.subtle.deriveKey(
+    {
+      name: "PBKDF2",
+      salt: new Uint8Array([
+        12, 12, 225, 1, 149, 230, 122, 103, 243, 211, 40, 176,
+      ]),
+      iterations: 1000000,
+      hash: "SHA-256",
+    },
+    keyMaterial,
+    { name: "AES-GCM", length: 256 },
+    true,
+    ["encrypt", "decrypt"]
+  );
+}
+
+const textEncoder = new TextEncoder();
+const textDecoder = new TextDecoder();
+const initializationVector = new Uint8Array([
+  207, 135, 15, 169, 75, 41, 3, 154, 88, 138, 122, 188,
+]);
--- src/routes/manfred.tsx
+++ src/routes/manfred.tsx
@@ -1,16 +1,67 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import PasswordGate from "../components/PasswordGate";
+import { encrypt, decrypt } from "../encryption";
 
 function Manfred() {
-  const [password, setPassword] = useState("");
+  const [password, setPassword] = useState<string | null>(null);
+  const [payload, setPayload] = useState("");
+  const [encryptedData, setEncryptedData] = useState<Uint8Array | null>(null);
+  const [decryptedData, setDecryptedData] = useState<string | null>(null);
+
+  useEffect(() => {
+    const encryptData = async () => {
+      if (password === null) {
+        return;
+      }
+      const encryptedData = await encrypt({ password, data: payload });
+      setEncryptedData(encryptedData);
+    };
+
+    encryptData();
+  }, [password, payload]);
+
+  useEffect(() => {
+    const decryptData = async () => {
+      if (password === null) {
+        return;
+      }
+      if (encryptedData === null) {
+        return;
+      }
+      const decryptedData = await decrypt({
+        password,
+        encryptedData,
+      });
+      setDecryptedData(decryptedData);
+    };
+
+    decryptData();
+  }, [password, encryptedData]);
 
   return (
     <div>
-      {password === "" ? (
+      {password === null ? (
         <PasswordGate onSubmit={setPassword} />
       ) : (
-        <p>You entered {password}</p>
+        <>
+          <input
+            type="text"
+            value={payload}
+            onChange={(e) => setPayload(e.target.value)}
+          />
+          <p>Password: {password}</p>
+          {encryptedData === null ? (
+            <p>Encrypted data is null</p>
+          ) : (
+            <p>Encrypted payload: {encryptedData.join(", ")}</p>
+          )}
+          {decryptedData === null ? (
+            <p>Decrypted data is null</p>
+          ) : (
+            <p>Decrypted payload: {decryptedData}</p>
+          )}
+        </>
       )}
     </div>
   );
