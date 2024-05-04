d9ec39d6ff56c781b02154a7c1c3aa1be02d2221
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-05-04 14:43:01 +0200

    refactor(manfred): move decryption up in logic

--- src/components/ManfredChat.tsx
+++ src/components/ManfredChat.tsx
@@ -1,33 +1,12 @@
 import { useState, useEffect } from "react";
-import Groq from "groq-sdk";
 
-import { decrypt } from "../utils/encryption";
-import { Message, getManfredResponse } from "../utils/manfred";
+import { Clients, Message, getManfredResponse } from "../utils/manfred";
 
 import "./ManfredChat.css";
 
-export default function ManfredChat({
-  encryptionKey,
-}: {
-  encryptionKey: CryptoKey;
-}) {
-  const [groqClient, setGroqClient] = useState<Groq | null>(null);
-
+export default function ManfredChat({ clients }: { clients: Clients }) {
   const [wipMessage, setWipMessage] = useState("");
   const [messages, setMessages] = useState<Message[]>([]);
-
-  useEffect(() => {
-    const decryptKeys = async () => {
-      const decryptedGroqKey = await decrypt({
-        key: encryptionKey,
-        encryptedData: encryptedGroqKey,
-      });
-      setGroqClient(
-        new Groq({ dangerouslyAllowBrowser: true, apiKey: decryptedGroqKey })
-      );
-    };
-    decryptKeys();
-  }, [encryptionKey]);
 
   const handleSendMessage = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -43,9 +22,6 @@ export default function ManfredChat({
   };
 
   useEffect(() => {
-    if (groqClient === null) {
-      return; // TODO: handle this
-    }
     if (messages.length === 0) {
       return;
     }
@@ -55,13 +31,13 @@ export default function ManfredChat({
 
     const addManfredResponse = async () => {
       const response = await getManfredResponse({
-        groqClient,
+        groqClient: clients.groq,
         messages,
       });
       setMessages([...messages, response]);
     };
     addManfredResponse();
-  }, [groqClient, messages]);
+  }, [clients, messages]);
 
   return (
     <div className="container">
@@ -85,11 +61,3 @@ export default function ManfredChat({
     </div>
   );
 }
-
-const encryptedGroqKey = new Uint8Array([
-  185, 49, 77, 231, 222, 82, 206, 134, 132, 50, 123, 100, 200, 242, 71, 107, 95,
-  111, 4, 10, 198, 112, 59, 248, 14, 249, 236, 63, 80, 219, 236, 83, 97, 198,
-  63, 114, 111, 202, 8, 44, 168, 187, 180, 62, 93, 195, 213, 154, 244, 58, 253,
-  105, 110, 129, 29, 218, 171, 21, 37, 40, 67, 170, 65, 208, 52, 152, 163, 183,
-  198, 168, 28, 128,
-]);
--- src/routes/manfred.tsx
+++ src/routes/manfred.tsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { Clients, decryptClients } from "../utils/manfred";
 
 import ManfredChat from "../components/ManfredChat";
 import PasswordGate from "../components/PasswordGate";
 
 function Manfred() {
   const [encryptionKey, setEncryptionKey] = useState<CryptoKey | null>(null);
+  const [clients, setClients] = useState<Clients | null>(null);
+
+  useEffect(() => {
+    const decryptKeys = async () => {
+      if (encryptionKey === null) {
+        return;
+      }
+      const decryptedClients = await decryptClients(encryptionKey);
+      setClients(decryptedClients);
+    };
+    decryptKeys();
+  }, [encryptionKey]);
 
   return encryptionKey === null ? (
     <PasswordGate setCorrectEncryptionKey={setEncryptionKey} />
+  ) : clients === null ? (
+    <div>Decrypting API clients...</div>
   ) : (
-    <ManfredChat encryptionKey={encryptionKey} />
+    <ManfredChat clients={clients} />
   );
 }
 
