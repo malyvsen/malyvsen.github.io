b7940577a1fedff555334d48e24497652a57d1ff
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-05-19 22:24:10 +0200

    fix(aktor): make photo appear in builds

--- src/aktor/FaceCutout.tsx
+++ src/aktor/FaceCutout.tsx
@@ -1,7 +1,9 @@
+import faceCutout from "./face-cutout.png";
+
 export default function FaceCutout({ style }: { style: React.CSSProperties }) {
   return (
     <img
-      src="src/aktor/face-cutout.png"
+      src={faceCutout}
       style={{
         marginTop: "-9%",
         marginRight: "-10%",
