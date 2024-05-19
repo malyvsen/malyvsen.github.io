e61a6eaa2fa4bafb01285e87a06837215457f9e9
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-05-19 19:46:37 +0200

    refactor(aktor): break page into components

---
+++ src/aktor/Hello.tsx
@@ -0,0 +1,15 @@
+export default function Hello() {
+  return (
+    <div style={{ marginLeft: "2em", marginRight: "2em" }}>
+      <h1>Mikołaj Bocheński</h1>
+      <p style={{ fontSize: "1.5em", textAlign: "justify" }}>
+        Cześć! Jestem 25-letnim aktorem z Krakowa. Mówię z dobrym akcentem po
+        polsku, angielsku (dzieciństwo spędziłem w Kalifornii), niemiecku,
+        holendersku, szwedzku i włosku. Mam doświadczenie przed kamerą i na
+        scenie, również musicalowej. Skończyłem roczne studium aktorstwa
+        filmowego na Mulholland Academy w Amsterdamie, właśnie kończę pierwszy
+        rok Szkoły Wokalno-Aktorskiej w Krakowie.
+      </p>
+    </div>
+  );
+}
--- src/aktor/index.tsx
+++ src/aktor/index.tsx
@@ -1,5 +1,9 @@
 import { useEffect } from "react";
 
+import Cutout from "./FaceCutout";
+import Hello from "./Hello";
+import Links from "./Links";
+
 export default function Aktor() {
   useEffect(() => {
     document.title = "Mikołaj Bocheński";
@@ -12,55 +16,14 @@ export default function Aktor() {
         flexDirection: "column",
         justifyContent: "space-between",
         alignItems: "center",
-        height: "100vh",
+        minHeight: "100vh",
         backgroundColor: "#ece0d7",
       }}
     >
-      <div style={{ marginTop: "1em" }}>
-        <h1 style={{ textAlign: "center" }}>Mikołaj Bocheński</h1>
-        <p style={{ fontSize: "1.5em", textAlign: "justify", margin: "2em" }}>
-          Cześć! Jestem 25-letnim aktorem z Krakowa. Mówię z dobrym akcentem po
-          polsku, angielsku (dzieciństwo spędziłem w Kalifornii), niemiecku,
-          holendersku, szwedzku i włosku. Mam doświadczenie przed kamerą i na
-          scenie, również musicalowej. Skończyłem roczne studium aktorstwa
-          filmowego na Mulholland Academy w Amsterdamie, właśnie kończę pierwszy
-          rok Szkoły Wokalno-Aktorskiej w Krakowie.
-        </p>
-      </div>
+      <Hello />
       <div style={{ display: "flex", width: "100%" }}>
-        <img
-          src="src/aktor/cutout.png"
-          style={{
-            width: "60%",
-            marginTop: "-48px",
-            marginRight: "-32px",
-            backgroundColor: "inherit",
-          }}
-        />
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-          }}
-        >
-          <p
-            style={{
-              fontSize: "1.5em",
-              textAlign: "justify",
-            }}
-          >
-            <a href="https://www.facebook.com/malyvsen">Facebook</a>
-            <br />
-            <a href="https://photos.app.goo.gl/yFrqRuZ5tNMAWF5MA">
-              Galeria zdjęć
-            </a>
-            <br />
-            <a href="https://www.youtube.com/watch?v=DupTZI6PZTQ">
-              Demo filmowe
-            </a>
-          </p>
-        </div>
+        <Cutout style={{ width: "60%" }} />
+        <Links />
       </div>
     </div>
   );
