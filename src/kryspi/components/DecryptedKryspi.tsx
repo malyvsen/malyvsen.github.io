a9b9731eacb0b864403aa982ca9806b1baaa69e2
malyvsen <5940672+malyvsen@users.noreply.github.com>
2024-07-21 14:38:26 +0200

    refactor(kryspi): move decrypted page to its own component

---
+++ src/kryspi/components/DecryptedKryspi.tsx
@@ -0,0 +1,46 @@
+import Clients from "@utils/clients";
+import useNow from "@utils/useNow";
+
+import ManfredChat from "../../manfred/components/ManfredChat";
+
+export default function DecryptedKryspi({ clients }: { clients: Clients }) {
+  const now = useNow();
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "flex-start",
+          width: "50%",
+          height: "100%",
+        }}
+      >
+        <ManfredChat clients={clients} />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          width: "50%",
+          height: "100%",
+        }}
+      >
+        <h1>
+          {String(now.getHours()).padStart(2, "0")}:
+          {String(now.getMinutes()).padStart(2, "0")}
+        </h1>
+      </div>
+    </div>
+  );
+}
--- src/kryspi/index.tsx
+++ src/kryspi/index.tsx
@@ -2,18 +2,15 @@ import { useState } from "react";
 import { useTitle } from "react-use";
 
 import Clients from "@utils/clients";
-import useNow from "@utils/useNow";
+import PasswordGate from "@utils/PasswordGate";
 
-import ManfredChat from "../manfred/components/ManfredChat";
-import PasswordGate from "../utils/PasswordGate";
-
+import DecryptedKryspi from "./components/DecryptedKryspi";
 import ThemeProvider from "./components/ThemeProvider";
 
 function Kryspi() {
   useTitle("Kryspinów");
 
   const [clients, setClients] = useState<Clients | null>(null);
-  const now = useNow();
 
   if (clients === null) {
     return (
@@ -25,41 +22,7 @@ function Kryspi() {
 
   return (
     <ThemeProvider>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          width: "100vw",
-          height: "100vh",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "flex-start",
-            width: "50%",
-            height: "100%",
-          }}
-        >
-          <ManfredChat clients={clients} />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            width: "50%",
-            height: "100%",
-          }}
-        >
-          <h1>
-            {String(now.getHours()).padStart(2, "0")}:
-            {String(now.getMinutes()).padStart(2, "0")}
-          </h1>
-        </div>
-      </div>
+      <DecryptedKryspi clients={clients} />
     </ThemeProvider>
   );
 }
