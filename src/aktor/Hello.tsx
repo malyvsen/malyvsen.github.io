4b18605cd697be610069f2b36b88868ce3eb5b77
malyvsen <5940672+malyvsen@users.noreply.github.com>
2026-01-23 10:02:42 +0100

    feat(aktor): update self-description

--- src/aktor/Hello.tsx
+++ src/aktor/Hello.tsx
@@ -3,12 +3,13 @@ export default function Hello() {
     <>
       <h1>Mikołaj Bocheński</h1>
       <p style={{ fontSize: "1.4em", textAlign: "justify" }}>
-        Cześć! Jestem 26-letnim aktorem z Krakowa. Mówię z dobrym akcentem po
-        polsku, angielsku (dzieciństwo spędziłem w Kalifornii), niemiecku,
-        holendersku, szwedzku i włosku. Mam doświadczenie przed kamerą i na
-        scenie, również musicalowej. Skończyłem roczne studium aktorstwa
-        filmowego na Mulholland Academy w Amsterdamie, a obecnie trwa drugi rok
-        mojej edukacji w Szkole Wokalno-Aktorskiej w Krakowie.
+        Cześć! Jestem 27-letnim aktorem z Krakowa. Mam doświadczenie przed
+        kamerą i na scenie, również muzycznej. Gram w teatrze Loch Camelot.
+        Mówię z dobrym akcentem po polsku, angielsku (dzieciństwo spędziłem w
+        Kalifornii), niemiecku, holendersku, szwedzku i włosku. Skończyłem
+        roczne studium aktorstwa filmowego na Mulholland Academy w Amsterdamie,
+        a obecnie trwa trzeci rok mojej edukacji w Szkole Wokalno-Aktorskiej w
+        Krakowie.
       </p>
     </>
   );
