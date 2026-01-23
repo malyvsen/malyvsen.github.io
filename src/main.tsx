d03bc90c71179ec936c76738575069f836aa6f7b
malyvsen <5940672+malyvsen@users.noreply.github.com>
2026-01-23 09:24:48 +0100

    feat(kryspi, manfred, szyfrownica): remove modules

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,21 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Aktor from "./aktor";
 import Home from "./home";
-import Kryspi from "./kryspi";
-import Manfred from "./manfred";
 import Programista from "./programista";
 import ArticlePage from "./programista/ArticlePage";
-import Szyfrownica from "./szyfrownica";
 
 const router = createBrowserRouter([
   { path: "/aktor", element: <Aktor /> },
   { path: "/", element: <Home /> },
-  { path: "/kryspi", element: <Kryspi /> },
-  { path: "/manfred", element: <Manfred /> },
   { path: "/programista", element: <Programista /> },
   { path: "/programista/:articleId", element: <ArticlePage /> },
-  { path: "/szyfrownica", element: <Szyfrownica /> },
 ]);
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@components": "/src/components",
-      "@utils": "/src/utils",
     },
   },
 });
