2f22f54534e77a7075b2411684751ec65cf859a3
malyvsen <5940672+malyvsen@users.noreply.github.com>
2026-01-25 23:08:41 +0100

    feat: generate page previews for social media

---
+++ meta.config.js
@@ -0,0 +1,41 @@
+/**
+ * Open Graph meta configuration for link previews.
+ */
+export default {
+  "/aktor": {
+    title: "Mikołaj Bocheński",
+    description: "Aktor",
+  },
+  "/pisarz": {
+    title: "Mikołaj Bocheński",
+    description: "Pisarz",
+  },
+  "/pisarz/sztuka-a-sztuczna-inteligencja": {
+    title: "Sztuka a sztuczna inteligencja",
+    description: "Przemyślenia aktora i programisty",
+  },
+  "/pisarz/byc-soba": {
+    title: "Być sobą",
+    description: "Jak nie dostać się do Ivy League",
+  },
+  "/pisarz/dwa-i-pol-procent": {
+    title: "Dwa i pół procent",
+    description: "O sztuce projektowania reguł",
+  },
+  "/programista": {
+    title: "Mikołaj Bocheński",
+    description: "Programista",
+  },
+  "/programista/research-code": {
+    title: "Research code",
+    description: "On the phases of machine learning software",
+  },
+  "/programista/polonizacja-programowania": {
+    title: "Polonizacja programowania",
+    description: "Rodzime zastępniki językowych nowotworów",
+  },
+  "/programista/perspective-on-llms": {
+    title: "A perspective on how LLMs work",
+    description: "If psychologists studied LLMs",
+  },
+};
